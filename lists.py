63840bbc3feb8c0fb5358ac38c719b92d4043a66
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-09 22:25:16 +0100

    more on lists.py

--- lists.py
+++ lists.py
@@ -3,7 +3,22 @@
 # a simple integer list
 integerList = [5, 6, 7]
 
+# printing an elment in a Lists
+print(integerList[0])
+
 
 # valid with multiple data types in a list including another list!
 multipleList = [5, "Hello", 10, "World", True, False, [5, 5, 5], integerList]
 print(multipleList)
+
+
+# Unlike Strings, Lists are mutable
+
+beforeList = ['h', 'e', 'l', 'l', 'o']
+print("Before changes: ")
+print(beforeList)
+
+# change an indexes
+beforeList[0] = 'T'
+print("After changes: ")
+print(beforeList)
