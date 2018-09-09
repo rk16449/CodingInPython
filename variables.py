c29e406df79695808910005030f5ccdd524814b9
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-09 21:43:26 +0100

    added file showing basics of variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+# variable assignment in python
+
+# create variable called car, set it with value 5
+car = 5
+print(car)
+
+# multiplying variables
+print(car * car)
+
+# python variable names cannot start with a number
+# 2Cars is invalid
+# two_cars is valid (snake casing)
+
+
+# variables cannot start with symbols
+# !@hello is invalid
