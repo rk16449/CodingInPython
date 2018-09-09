73b99659f17efdfb7d06e180fdf0676c7c2f2f47
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-09 21:38:48 +0100

    order of calculation in python

--- numbers.py
+++ numbers.py
@@ -1,1 +1,8 @@
-print(2+10);
+# order of operation test
+
+# here: 16*15 first then 4 + 5 = 249
+print(4+16 * 15+5)
+
+# with brackets, the calculation changes
+# 20 * 20 = 400
+print( (4+16) * (15+5))
