234050193c2a12e45d312dd8b026cd86df5c57d5
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-09 22:23:11 +0100

    simple list experimenting

---
+++ lists.py
@@ -0,0 +1,9 @@
+# Lists can be thought of as Pythons arrays
+
+# a simple integer list
+integerList = [5, 6, 7]
+
+
+# valid with multiple data types in a list including another list!
+multipleList = [5, "Hello", 10, "World", True, False, [5, 5, 5], integerList]
+print(multipleList)
