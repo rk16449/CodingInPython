5f37831a17116832517c0cfc3f5d9433c424084d
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-11 22:27:26 +0100

    updated lists

--- lists.py
+++ lists.py
@@ -22,3 +22,36 @@ print(beforeList)
 beforeList[0] = 'T'
 print("After changes: ")
 print(beforeList)
+
+
+# poping a Lists
+last_element = beforeList.pop()
+print(last_element)
+
+
+# reversing a List
+beforeList.reverse()
+print(beforeList)
+
+
+
+# sorting a List
+beforeList.sort()
+print(beforeList)
+
+# nested lists
+nested_list_one = [5, 6, 7, [4, 5, 6]]
+# print out the nested list since its at index 3
+print(nested_list_one[3])
+
+# print get the index 1 inside the nested list
+print(nested_list_one[3][1])
+
+# list comprehensions
+nested_list_two = [[1, 2, 3], [4,5,6], [7,8,9]]
+
+# get the first row values
+first_row = [row[0] for row in nested_list_two]
+
+# this will return [1, 4, 7]
+print(first_row)
